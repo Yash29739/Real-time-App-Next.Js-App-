23fa4b529c371064a3c364542cd6eb8704ef1d66
B_M_Yashwanth <bhagyapushpagiri17@gmail.com>
2025-08-15 14:04:42 +0530

    update: minor changes that had been left out to be updated

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,16 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/ui/Header'
 import React from 'react'
 
 const Document = () => {
   return (
-    <div>Document</div>
+    <div>
+      <Header>
+        <p>Test</p>
+      </Header>
+      <Editor/>
+    </div>
   )
 }
 
-export default Document
+export default Document  
